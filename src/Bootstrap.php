b0c509c12faa990d48fda493fd5eaa6bf3cc4f94
Patrick <patrick@peakinformatik.com>
2014-09-10 20:05:51 +0200

    added the routing

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,13 +1,16 @@
 <?php
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 error_reporting(E_ALL);
 
-$debug = true; // todo: set depending on environment
+$environment = 'development'; // todo: detect environment
 
+/**
+ * Register the error handler
+ */
 $woops = new \Whoops\Run;
-if ($debug) {
+if ($environment === 'development') {
     $woops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 } else {
     $woops->pushHandler(function($e){
@@ -19,4 +22,50 @@ if ($debug) {
 }
 $woops->register();
 
-throw new Exception;
+/**
+ * Set up the http component
+ */
+$cookieBuilder = new Http\CookieBuilder;
+$cookieBuilder->setDefaultSecure($environment === 'production');
+
+$request = new Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new Http\HttpResponse;
+
+/**
+ * Set up the routing
+ */
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/test', function(){
+        echo 'hello world';
+    });
+});
+
+$routeInfo = $dispatcher->dispatch(
+    $request->getMethod(), 
+    $_SERVER['REQUEST_URI']
+);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        $response->setStatusCode(404);
+        $response->setContent('404 - Page not found');
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $response->setStatusCode(405);
+        $response->setContent('405 - Method not allowed');
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        $handler($vars);
+        break;
+}
+
+/**
+ * Send the http response
+ */
+foreach ($response->getHeaders() as $header) {
+    header($header);
+}
+
+echo $response->getContent();
