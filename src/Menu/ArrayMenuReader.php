2e533140de473b1adff42e7ae809c545fdf5e8d9
Patrick <patrick@peakinformatik.com>
2015-09-03 21:21:47 +0200

    next chapter (experimental)

--- src/Menu/ArrayMenuReader.php
+++ src/Menu/ArrayMenuReader.php
@@ -2,10 +2,36 @@
 
 namespace Example\Menu;
 
+use Http\Request;
+
 class ArrayMenuReader implements MenuReader
 {
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
     public function readMenu()
     {
-        return [['href' => '/', 'text' => 'Homepage']];
+        $menuItems = [
+            ['href' => '/', 'text' => 'Homepage'],
+            ['href' => '/page-one', 'text' => 'Page One'],
+        ];
+
+        foreach ($menuItems as $key => $item) {
+            if ($item['href'] === '/' . $this->getFirstPathSegment()) {
+                $menuItems[$key]['active'] = true;
+            }
+        }
+
+        return $menuItems;
+    }
+
+    private function getFirstPathSegment()
+    {
+        $path = $this->request->getPath();
+        return strtok($path, '/');
     }
 }
